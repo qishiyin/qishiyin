7bc1043a8285c6793d566e8e473d2fce223aff8c
qishiyin <67813216+qishiyin@users.noreply.github.com>
2022-11-06 19:15:36 +0800

    0.1
    对窗体的时装

--- TextGame/src/com/textgame/anluxi/UI/AnFrame.java
+++ TextGame/src/com/textgame/anluxi/UI/AnFrame.java
@@ -1,39 +1,182 @@
 package com.textgame.anluxi.UI;
 
+import java.util.ArrayList;
+
 public class AnFrame extends UI
 {
 	//标题
 	public String titleString = null;
-	
+	//显示窗口
 	public String showframeString;
-	public char[] chargroup;
+	//长宽组
+	public ArrayList<String> hchargroup,vchargroup,vstringgroup,threestringgroup;
+	//输出的片段
+	public String copystring,zerostring,twostring,endstring,voidstring;
 	
 	public String ui(float x,float y,int width,int height)
 	{
 		//╔ ╗╝╚║═
-		//╔═════╗
-		//║     ║
-		//║     ║
-		//╚═════╝
+		//╔════════════════════╗
+		//║标题              - x ║
+		//║------------------ -║
+		//║                    ║
+		//║                    ║
+		//║                    ║
+		//║                    ║
+		//║                    ║
+		//║                    ║
+		//╚════════════════════╝
 		//如果是正方形则width = height +1
 		//无法制作单位为一的正方形
 		//
 		//
-		//showframeString = "" + lefttop + hbar+righttop+"\n"+vbar+" "+vbar+"\n"+leftdown+hbar+rightdown;
-		if(titleString != null)
+		hchargroup = new ArrayList<String>();
+		vchargroup = new ArrayList<String>();
+		vstringgroup = new ArrayList<String>();
+		threestringgroup = new ArrayList<String>();
+		copystring = "";
+		zerostring = "";
+		twostring = "";
+		endstring = "";
+		voidstring = "";
+		
+		for(int  h= 0;h<width;h++)
 		{
-			showframeString = ""+vbars+hbars+vbars+"\n"+vbars+ titleString+vbars+"\n"+vbars+hbars+vbars;
+			hchargroup.add(""+hbar);
+			
 		}
-		else 
+		
+		for(int v = 0;v<height + 3;v++)
 		{
-			showframeString = ""+vbars+hbars+vbars+"\n"+vbars+ " "+vbars+"\n"+vbars+hbars+vbars;
+			vchargroup.add( "\n" + vbar);
 		}
+		
+		for(int i=0;i<width;i++)
+		{
+			vstringgroup.add(" ");
+			threestringgroup.add("-");
+			
+		}
+		if(titleString == null)
+		{
+			for(String s : hchargroup)
+			{
+				zerostring = zerostring + s;
+			}
+		}else
+		{
+			
+		}
+		for(String s : hchargroup)
+		{
+			
+			endstring = endstring + s;
+		}
+		if(titleString == null)
+		{
+			for(String s : vstringgroup)
+			{
+				voidstring = voidstring + s;
+			}
+		}else
+		{
+			int i = titleString.length();
+			for(int j = 0;j<vstringgroup.size() - i;j++)
+			{
+				voidstring = voidstring + vstringgroup.get(j);
+			}
+		}
+		
+		
+		for(String s : threestringgroup)
+		{
+			twostring = twostring + s;
+		}
+		
+		if(titleString == null)
+		{
+			
+			
+			for(int v = 0;v <= height;v++)
+			{
+				
+				if(v == 0)
+				{
+					copystring =   copystring + lefttop + zerostring + righttop + "\n";
+				}
+				if(v == 1)
+				{
+					if(titleString != null )
+					{
+						copystring = copystring + titleString + voidstring;
+					}
+				}
+				if(v == 2)
+				{
+					copystring =   copystring + vbar + twostring + vbar + "\n";
+				}
+				if(v == height  )
+				{
+					copystring =   copystring + leftdown + endstring + rightdown;
+				}
+				if(v != 0)
+				{
+					if(v != height)
+					{
+						copystring =  copystring + vbar + voidstring + vbar + "\n";
+					}
+					
+				}
+				
+				
+			}
+			
+		}
+		else
+		{
+			for(int v = 0;v <= height;v++)
+			{
+				
+				if(v == 0)
+				{
+					copystring =   copystring + lefttop + zerostring + righttop + "\n";
+				}
+				if(v == 2)
+				{
+					copystring =   copystring + vbar + twostring + vbar + "\n";
+				}
+				if(v == height  )
+				{
+					copystring =   copystring + leftdown + endstring + rightdown;
+				}
+				if(v != 0)
+				{
+					if(v != height)
+					{
+						copystring =  copystring + vbar + voidstring + vbar + "\n";
+					}
+					
+				}
+				
+				
+			}
+		}
+		showframeString = copystring;
+		//if(titleString != null)
+		//{
+		//	showframeString = ""+vbars+hbars+vbars+"\n"+vbars+ titleString+vbars+"\n"+vbars+hbars+vbars;
+		//}
+		//else 
+		//{
+		//	showframeString = ""+vbars+hbars+vbars+"\n"+vbars+ " "+vbars+"\n"+vbars+hbars+vbars;
+		//}
 		return showframeString;
 	}
 	
 	public boolean show()
 	{
 		String uiString = ui(x, y, width, height);
+		
 		System.out.println(uiString);
 		return true;
 	}
@@ -54,10 +197,7 @@ public class AnFrame extends UI
 		this.y = y;
 		this.width = width;
 		this.height = height;
-		for(int i=0;i<titleString.length();i++) 
-		{
-			chargroup[i] = '-';
-		}
+		
 		show();
 	}
 	
@@ -65,4 +205,28 @@ public class AnFrame extends UI
 	{
 		this.titleString = titleString;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
